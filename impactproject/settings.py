9f2603ec2d782cb4b35e4a473cb249c04db0755f
Ganeshivan <ganeshbalaji160@gmail.com>
2020-09-18 09:37:26 +0530

    good

--- impactproject/settings.py
+++ impactproject/settings.py
@@ -127,5 +127,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'asserts')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
-prod_db  =  dj_database_url.config(conn_max_age=500)
+prod_db  =  dj_database_url.config(conn_max_age=None)
 DATABASES['default'].update(prod_db)
