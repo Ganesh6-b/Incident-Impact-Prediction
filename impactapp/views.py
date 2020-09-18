b3dd31ec1a47f9a67400891539c8b88c9e1b7aa7
Ganeshivan <ganeshbalaji160@gmail.com>
2020-09-18 14:55:23 +0530

    Ini

--- impactapp/views.py
+++ impactapp/views.py
@@ -11,40 +11,43 @@ def home(request):
     return render(request, "home.html")
 
 def ImpactPrediction(request):
-    if request.method == 'POST':
-        try:
-            df = pd.read_csv(request.FILES['files'])
-        except:
-            df = pd.read_excel(request.FILES['files'], sheet_name=1)
-        fin_df = df[['ID', 'ID_status', 'count_reassign', 'count_updated', 'ID_caller','opened_by', 'Created_by', 'updated_by', 'location', 'category_ID','user_symptom', 'Support_group', 'support_incharge']]
-        fin_df['ID'] = fin_df['ID'].astype(str).str[3:].astype(int)
-        #Remove the extra part the attributes like "Opened by 8" to "8" for more understanding. do it for all attributes which have like this
-        fin_df['ID_caller'] = fin_df['ID_caller'].astype(str).str[7:].astype(int)
-        fin_df['opened_by'] = fin_df['opened_by'].astype(str).str[10:].astype(int)
-        fin_df['Created_by'] = fin_df['Created_by'].astype(str).str[11:].astype(int)
-        fin_df['updated_by'] = fin_df['updated_by'].astype(str).str[11:].astype(int)
-        fin_df['location'] = fin_df['location'].astype(str).str[9:].astype(int)
-        fin_df['category_ID'] = fin_df['category_ID'].astype(str).str[9:].astype(int)
-        fin_df['user_symptom'] = fin_df['user_symptom'].astype(str).str[8:].astype(int)
-        fin_df['Support_group'] = fin_df['Support_group'].astype(str).str[6:].astype(int)
-        fin_df['support_incharge'] = fin_df['support_incharge'].astype(str).str[9:].astype(int)
+    try:
+        if request.method == 'POST':
+            try:
+                df = pd.read_csv(request.FILES['files'])
+            except:
+                df = pd.read_excel(request.FILES['files'], sheet_name=1)
+            fin_df = df[['ID', 'ID_status', 'count_reassign', 'count_updated', 'ID_caller','opened_by', 'Created_by', 'updated_by', 'location', 'category_ID','user_symptom', 'Support_group', 'support_incharge']]
+            fin_df['ID'] = fin_df['ID'].astype(str).str[3:].astype(int)
+            #Remove the extra part the attributes like "Opened by 8" to "8" for more understanding. do it for all attributes which have like this
+            fin_df['ID_caller'] = fin_df['ID_caller'].astype(str).str[7:].astype(int)
+            fin_df['opened_by'] = fin_df['opened_by'].astype(str).str[10:].astype(int)
+            fin_df['Created_by'] = fin_df['Created_by'].astype(str).str[11:].astype(int)
+            fin_df['updated_by'] = fin_df['updated_by'].astype(str).str[11:].astype(int)
+            fin_df['location'] = fin_df['location'].astype(str).str[9:].astype(int)
+            fin_df['category_ID'] = fin_df['category_ID'].astype(str).str[9:].astype(int)
+            fin_df['user_symptom'] = fin_df['user_symptom'].astype(str).str[8:].astype(int)
+            fin_df['Support_group'] = fin_df['Support_group'].astype(str).str[6:].astype(int)
+            fin_df['support_incharge'] = fin_df['support_incharge'].astype(str).str[9:].astype(int)
 
-        ID_status_dict = {'New' : 0 , 'Resolved' : 1, 'Closed' : 2, 'Active': 3, 'Awaiting User Info': 4, 'Awaiting Problem' : 5, 'Awaiting Vendor' : 6, 'Awaiting Evidence' : 7, '-100' : 8}
-        fin_df['ID_status'] = fin_df['ID_status'].map(ID_status_dict)
+            ID_status_dict = {'New' : 0 , 'Resolved' : 1, 'Closed' : 2, 'Active': 3, 'Awaiting User Info': 4, 'Awaiting Problem' : 5, 'Awaiting Vendor' : 6, 'Awaiting Evidence' : 7, '-100' : 8}
+            fin_df['ID_status'] = fin_df['ID_status'].map(ID_status_dict)
 
-        fin_df['ID_status'] = fin_df['ID_status'].astype(int)
-        fin_df['count_reassign'] = fin_df['count_reassign'].astype(int)
-        fin_df['count_updated'] = fin_df['count_updated'].astype(int)
-        pred_val = np.array(classifier.predict(fin_df))
-        Id_val = np.array(fin_df['ID'])
-        fin_series = pd.Series(pred_val, index = Id_val)
-        dict_impact = {1 : "Low", 2 : "Medium", 3 : "High"}
-        fin_series = fin_series.map(dict_impact)
-        pred_dict = fin_series.to_dict()
-        context = {"pred_dict" : pred_dict}
-        return render(request, "result.html", context = context)
-    else:
-        return render(request, "home.html")
+            fin_df['ID_status'] = fin_df['ID_status'].astype(int)
+            fin_df['count_reassign'] = fin_df['count_reassign'].astype(int)
+            fin_df['count_updated'] = fin_df['count_updated'].astype(int)
+            pred_val = np.array(classifier.predict(fin_df))
+            Id_val = np.array(fin_df['ID'])
+            fin_series = pd.Series(pred_val, index = Id_val)
+            dict_impact = {1 : "Low", 2 : "Medium", 3 : "High"}
+            fin_series = fin_series.map(dict_impact)
+            pred_dict = fin_series.to_dict()
+            context = {"pred_dict" : pred_dict}
+            return render(request, "result.html", context = context)
+        else:
+            return render(request, "home.html")
+    except:
+        return HttpResponse("Oops!!! Your file contains wrong information. Check that file and their features and then try it....")
 def ImpactPredictionValue(request):  
     if request.method == 'POST':
         ID = int(request.POST['ID'])
